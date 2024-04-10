a0fa2b57bacd2c0a5fcbfa26a4d6a04f996f2d45
leandro basignani <leandrobasignani@gmail.com>
2024-04-10 00:23:35 -0300

    test

--- src/TechTalksModel/DTO/TechTalkDTO.cs
+++ src/TechTalksModel/DTO/TechTalkDTO.cs
@@ -1,7 +1,7 @@
 using System;
 // using System.ComponentModel.DataAnnotations;
 // using System.ComponentModel.DataAnnotations.Schema;
-// test
+// test 2
 namespace TechTalksModel.DTO
 {
     public class TechTalkDTO
